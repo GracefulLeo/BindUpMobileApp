be1e1e67499958fe09fc5c28f488da61b65ed98d
GracefulLeo <29eb1f1ca>
2018-05-29 15:04:45 +0300

    Added all methods for fragment transations into interface

--- app/src/main/java/com/example/rrty6/vcardapp/ui/interfaces/IMainActivity.java
+++ app/src/main/java/com/example/rrty6/vcardapp/ui/interfaces/IMainActivity.java
@@ -5,8 +5,14 @@ import com.example.rrty6.vcardapp.data.storage.model.Group;
 
 public interface IMainActivity   {
 
-    void  inflateViewProfileFragment(Card card);
+    void inflateViewProfileFragment(Card card);
     void inflateViewContactProfileFragment (Card card);
     void inflateViewGroupProfileGroups (Group group);
+    void inflateMyVCardFragment ();
+    void inflateGroupCreateFragment ();
+    void inflateGroupNoGroupsFragment ();
+    void inflateCreateCardFragment ();
+    void inflateVCardFirstLoginFragment ();
+    void inflateContactFragment ();
 
 }
